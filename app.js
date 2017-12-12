35049d788920de8af2c4fb3bb2a397ae21b25cd5
Taylor Stemple <taylorstemple@gmail.com>
2017-12-11 16:05:51 -0800

    got my arrays pushed!

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 'use strict';
 
-var hourlySales = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
-pikeSales = [];
-seaTacSales = [];
-seaCenSales = [];
-capHillSales = [];
-alkiSales = [];
+var storeHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 
 var pikeFirst = {
   custMin: 23,
   custMax: 65,
   avgCook: 6.3,
+  sales: [],
   actCust: function(min,max) {
     return Math.floor(Math.random() * (this.custMax - this.custMin) + this.custMin);
   }
@@ -20,6 +16,7 @@ var seaTac = {
   custMin: 3,
   custMax: 24,
   avgCook: 1.2,
+  sales: [],
   actCust: function(min,max) {
     return Math.floor(Math.random() * (this.custMax - this.custMin) + this.custMin);
   }
@@ -29,6 +26,7 @@ var seaCenter = {
   custMin: 11,
   custMax: 38,
   avgCook: 3.7,
+  sales: [],
   actCust: function(min,max) {
     return Math.floor(Math.random() * (this.custMax - this.custMin) + this.custMin);
   }
@@ -38,6 +36,7 @@ var capHill = {
   custMin: 20,
   custMax: 38,
   avgCook: 2.3,
+  sales: [],
   actCust: function(min,max) {
     return Math.floor(Math.random() * (this.custMax - this.custMin) + this.custMin);
   }
@@ -47,6 +46,7 @@ var alki = {
   custMin: 2,
   custMax: 16,
   avgCook: 4.6,
+  sales: [],
   actCust: function(min,max) {
     return Math.floor(Math.random() * (this.custMax - this.custMin) + this.custMin);
   }
@@ -54,4 +54,11 @@ var alki = {
 
 function getSales(storeName) {
   return Math.floor(storeName.actCust() * storeName.avgCook);
+}
+
+function hourlySales(store) {
+  for(var i = 0; i < storeHours.length; i++) {
+    store.sales.push(getSales(store));
+  }
+  console.log(store);
 }
