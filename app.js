44f29db59b5a469feb6620bf03fbe20d710984c9
Taylor Stemple <taylorstemple@gmail.com>
2017-12-12 21:20:59 -0800

    still trying to get data to print

--- app.js
+++ app.js
@@ -1,149 +1,68 @@
 'use strict';
 
+
+//Helper functions and arrays
+//**********************************************************************************************
 var storeHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 
 function ranNum(min,max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+var storeTable = document.getElementById('sales_table');
+
 var patsLocales = [];
 
+function renderSales() {
+  for( var i = 0; i < patsLocales.length; i++) {
+    patsLocales[i].render();
+  }
+}
+
+
 //Constructor function
+//**********************************************************************************************
+
 function Store(storeLocation, custMinPerHour, custMaxPerHour, avgCookPerCust) {
   this.storeLocation = storeLocation;
   this.custMinPerHour = custMinPerHour;
   this.custMaxPerHour = custMaxPerHour;
   this.avgCookPerCust = avgCookPerCust;
-  salesPerHour = [];
-  custsPerHour = [];
-  dailyTotalSales = 0;
-  calcHourlyCusts = function() {
+  this.salesPerHour = [];
+  this.custsPerHour = [];
+  this.dailyTotalSales = 0;
+  patsLocales.push(this);
+  this.calcHourlyCusts = function() {
     for( var i = 0; i < storeHours.length; i++) {
       this.custsPerHour.push(ranNum(this.custMinPerHour,this.custMaxPerHour))
     };
   };
-  calcSalesPerHour = function() {
+  this.calcSalesPerHour = function() {
     this.calcHourlyCusts();
     for(var i = 0; i < storeHours.length; i++) {
       this.salesPerHour.push(Math.ceil(this.custsPerHour[i] * this.avgCookPerCust))
     };
   };
-  patsLocales.push(this);
+
+  this.render = function() {
+    this.calcSalesPerHour();
+    var tdEl = document.createElement('td');
+    var trEl = document.createElement('tr');
+    tdEl.textContent = this.storeLocation;
+    trEl.appendChild(tdEl);
+
+    for(var i = 0; i < storeHours.length; i++) {
+      tdEl = document.createElement('td');
+      tdEl.textContent = this.salesPerHour[i];
+      trEl.appendChild(tdEl);
+    }
+
+    storeTable.appendChild(trEl);
+  };
+
 };
+
 
 new Store('First and Pike', 23, 65, 6.3);
 new Store('SeaTac Airport', 3, 24, 1.2);
 new Store('Seattle Center', 11, 38, 3.7);
-
-
-
-var seaTac = {
-  storeLocation: 'SeaTac Airport',
-  custMin: 3,
-  custMax: 24,
-  avgCook: 1.2,
-  sales: [],
-  actCust: function(min,max) {
-    return Math.floor(Math.random() * (this.custMax - this.custMin + 1) + this.custMin);
-  }
-}
-
-var seaCenter = {
-  storeLocation: 'Seattle Center',
-  custMin: 11,
-  custMax: 38,
-  avgCook: 3.7,
-  sales: [],
-  actCust: function(min,max) {
-    return Math.floor(Math.random() * (this.custMax - this.custMin + 1) + this.custMin);
-  }
-}
-
-var capHill = {
-  storeLocation: 'Capitol Hill',
-  custMin: 20,
-  custMax: 38,
-  avgCook: 2.3,
-  sales: [],
-  actCust: function(min,max) {
-    return Math.floor(Math.random() * (this.custMax - this.custMin + 1) + this.custMin);
-  }
-}
-
-var alki = {
-  storeLocation: 'Alki',
-  custMin: 2,
-  custMax: 16,
-  avgCook: 4.6,
-  sales: [],
-  actCust: function(min,max) {
-    return Math.floor(Math.random() * (this.custMax - this.custMin + 1) + this.custMin);
-  }
-}
-
-//functions
-
-//addition function
-function sum(a,b) {
-  return (a + b);
-}
-//function to determine number of cookies sold daily
-function dailyTotal(storeLoc) {
-    var arrSum = 0;
-
-  for(var i =0; i <storeLoc.sales.length; i++) {
-    arrSum = sum(arrSum,storeLoc.sales[i]);
-    }
-  return arrSum;
-  console.log(arrSum);
-
-}
-
-//function to determine cookies sold every hour
-function hourlySales(store) {
-  for(var i = 0; i < storeHours.length; i++) {
-    store.sales.push(getSales(store));
-  }
-  console.log(store);
-}
-
-
-function getSales(storeName) {
-  return Math.ceil(storeName.actCust() * storeName.avgCook);
-}
-
-function printSales(store) {
-
-  hourlySales(store);
-  var totalSales = dailyTotal(store);
-
-
-
-  var container = document.createElement('div');
-  container.innerHTML = '<p>' + store.storeLocation + ' Hourly Sales</p>';
-  document.body.appendChild(container);
-
-  var salesList = document.createElement('ul');
-  var listArr = [];
-
-  for(var i = 0; i < storeHours.length; i++) {
-    listArr.push('<li>' + storeHours[i] + ': ' + store.sales[i] + '</li>');
-  }
-
-  listArr.push('<li> Daily total: ' + totalSales + '</li>');
-  var fullList = listArr.join('');
-
-  salesList.innerHTML = fullList;
-  document.body.appendChild(salesList);
-
-
-}
-
-//code from samhamm
-
-calcCookiesEachHour: function() {
-  this.calcCustsEachHour();
-  for(var i = 0; i < hours.length; i++) {
-    var oneHour =Math.ceil(this.custsEachHour[i] * this.avgCookiesPerHour);
-  }
-}
